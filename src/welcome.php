c1e625b12b2ad1aed1ff2a5181a4687a5353f701
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-13 23:08:27 +0200

    Create welcome.php

---
+++ src/welcome.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    header("Location: index.html");
+    exit;
+}
+echo "Bienvenue " . $_SESSION['username'] . "!";
